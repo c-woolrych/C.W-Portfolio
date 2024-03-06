8f59d037881c744a0b58842a6a6a35727c978cc6
c-woolrych <c.woolrych@gmail.com>
2024-03-06 12:43:39 +0000

    fix navbar and create pages folder for pages

--- C.W-Portfolio/src/components/Navtabs.jsx
+++ C.W-Portfolio/src/components/Navtabs.jsx
@@ -1,43 +1,30 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const navbar = {
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'pink'
+}
+
 
 function NavTabs() {
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <NavLink
-                    to="/"
-                    end
-                    className={({ isActive }) =>
-                        isActive ? 'nav-link active' : 'nav-link'
-                    }
-                >
-                    Home
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink
-                    to="projects"
-                    className={({ isActive }) =>
-                        isActive ? 'nav-link active' : 'nav-link'
-                    }
-                >
-                    Projects
-                </NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink
-                    to="contact"
-                    end
-                    className={({ isActive }) =>
-                        isActive ? 'nav-link active' : 'nav-link'
-                    }
-                >
-                    Contact
-                </NavLink>
-            </li>
-        </ul>
-    );
+        <>
+            <Navbar justify expand="sm" bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Nav className="nav nav-pills">
+                        <NavLink to="/"end className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}  >Home</NavLink>
+                        <NavLink to="projects" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Projects</NavLink>
+                        <NavLink to="contact" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>Contact</NavLink>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
 }
 
 export default NavTabs;
